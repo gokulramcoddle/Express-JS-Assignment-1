378adb8215e400a03514d1fc120b6da2aa31d53c
gokulramcoddle <gokulram.t@coddletech.com>
2025-04-18 18:35:59 +0530

    Changed DB location

--- models/applicationModel.js
+++ models/applicationModel.js
@@ -1,4 +1,4 @@
-const db = require('./db');
+const db = require('../config/db');
 
 const usersApplication = async() => {
    const [usersApplication] = await db.query(
--- models/userModel.js
+++ models/userModel.js
@@ -1,4 +1,4 @@
-const db = require('./db');
+const db = require('../config/db');
 
 const usersData = async() => {
     const [users] = await db.query('SELECT firstname, lastname, email FROM users');
